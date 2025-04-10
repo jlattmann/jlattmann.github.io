ba6209be7e407a9949fa958bdf1bd096eb3890ad
jlattmann <85623566+jlattmann@users.noreply.github.com>
2025-04-10 06:27:51 +0000

    Deploying to gh-pages from @ jlattmann/jlattmann.github.io@96f6cb2a9445a39ff3a753ab560f112b2078f992 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,11 +24,11 @@ ninja.data = [{
             window.location.href = "/workingpapers/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/CV/";
           },
         },{id: "post-a-post-with-plotly-js",
         
